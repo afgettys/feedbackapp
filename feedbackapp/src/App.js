53ccc836200ed192a97c7bf9f7347fc78faba323
Amanda Gettys <afgettys@gmail.com>
2022-03-18 16:27:21 -0400

    adding router

--- feedbackapp/src/App.js
+++ feedbackapp/src/App.js
@@ -1,26 +1,31 @@
 import { useState } from "react";
+import {v4 as uuidv4} from 'uuid';
 import Header from "./components/Header";
 import FeedbackList from "./components/FeedbackList";
 import FeedbackStats from "./components/FeedbackStats";
 import FeedbackForm from "./components/FeedbackForm";
 import FeedbackData from "./data/FeedbackData";
 
-
 function App() {
   const [feedback, setFeedback] = useState(FeedbackData);
-  const deleteFeedback = (id) =>{
-    if (window.confirm ('are you sure?')){
-      setFeedback(feedback.filter((item) => item.id !== id))
-    }
+
+  const addFeedback = (newFeedback) =>{
+    newFeedback.id = uuidv4()
+    setFeedback ([newFeedback,...feedback])
+    console.log(newFeedback)
   }
+  const deleteFeedback = (id) => {
+    if (window.confirm("are you sure?")) {
+      setFeedback(feedback.filter((item) => item.id !== id));
+    }
+  };
   return (
     <>
       <Header />
       <div className="container">
-        <FeedbackForm />
-        <FeedbackStats feedback={feedback}/>
-        <FeedbackList feedback={feedback} 
-        handleDelete ={deleteFeedback}/>
+        <FeedbackForm handleAdd={addFeedback} />
+        <FeedbackStats feedback={feedback} />
+        <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
       </div>
     </>
   );
