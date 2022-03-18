2cb772c636ed07fd4e0ed2f7832726cb42795a4e
Amanda Gettys <afgettys@gmail.com>
2022-03-18 17:15:53 -0400

    uploading router with issues"
    git add .
    clear
    git add .

--- feedbackapp/src/App.js
+++ feedbackapp/src/App.js
@@ -1,33 +1,36 @@
 import { useState } from "react";
-import {v4 as uuidv4} from 'uuid';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { v4 as uuidv4 } from "uuid";
 import Header from "./components/Header";
 import FeedbackList from "./components/FeedbackList";
 import FeedbackStats from "./components/FeedbackStats";
 import FeedbackForm from "./components/FeedbackForm";
+import AboutPage from "./pages/AboutPage";
 import FeedbackData from "./data/FeedbackData";
 
 function App() {
   const [feedback, setFeedback] = useState(FeedbackData);
 
-  const addFeedback = (newFeedback) =>{
-    newFeedback.id = uuidv4()
-    setFeedback ([newFeedback,...feedback])
-    console.log(newFeedback)
-  }
+  const addFeedback = (newFeedback) => {
+    newFeedback.id = uuidv4();
+    setFeedback([newFeedback, ...feedback]);
+    console.log(newFeedback);
+  };
   const deleteFeedback = (id) => {
     if (window.confirm("are you sure?")) {
       setFeedback(feedback.filter((item) => item.id !== id));
     }
   };
   return (
-    <>
+    <Router>
       <Header />
       <div className="container">
         <FeedbackForm handleAdd={addFeedback} />
         <FeedbackStats feedback={feedback} />
         <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
+        <Route path="/about" component={AboutPage} />
       </div>
-    </>
+    </Router>
   );
 }
 
